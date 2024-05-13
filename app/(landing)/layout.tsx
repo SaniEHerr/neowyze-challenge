28ba0a5b24fe01e44ccdebe9fd95433aad10906e
SaniEHerr <santiagoeherrera@gmail.com>
2024-05-13 13:50:32 -0300

    Add: (landing) layout and page structure and styles

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,11 +1,20 @@
+import Footer from "../_components/Footer"
+import Navbar from "../_components/Navbar"
+
 const LandingLayout = ({
-  children
+  children,
 }: {
   children: React.ReactNode
 }) => {
   return (
-    <div>
-      {children}
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <Navbar />
+
+      <main className="max-w-[1280px] flex flex-col justify-center items-center flex-1">
+        {children}
+      </main>
+
+      <Footer />
     </div>
   )
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,64 @@
-const LandingPage = () => {
-  return (
-    <div>
-      LandingPage
-    </div>
-  )
-}
+"use client"
 
-export default LandingPage
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [hoveredFilm, setHoveredFilm] = useState(false);
+  const [hoveredCharacter, setHoveredCharacter] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 768);
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleFilmHover = () => {
+    if (!isMobile) {
+      setHoveredFilm(!hoveredFilm);
+    }
+  };
+
+  const handleCharacterHover = () => {
+    if (!isMobile) {
+      setHoveredCharacter(!hoveredCharacter);
+    }
+  };
+
+  return (
+    <main className="flex flex-col md:flex-row items-center justify-center gap-8 mx-auto font-starjedi py-4 px-0">
+      <Link href="/films">
+        <div
+          className="bg-zinc-700 bg-opacity-60 backdrop-filter backdrop-blur-sm p-5 text-4xl flex justify-center items-center w-[291px] md:w-[300px] h-[200px] md:h-[400px] hover:bg-zinc-600 transition duration-300 ease-in-out rounded-md"
+          onMouseEnter={handleFilmHover}
+          onMouseLeave={handleFilmHover}
+        >
+          <p>
+            {hoveredFilm || isMobile ? (isMobile ? "Tap" : "Click") + " for know more about films" : "Films"}
+          </p>
+        </div>
+      </Link>
+
+      <Link href="/characters">
+        <div
+          className="bg-zinc-700 bg-opacity-60 backdrop-filter backdrop-blur-sm p-5 text-4xl flex justify-center items-center w-[291px] md:w-[300px] h-[200px] md:h-[400px] hover:bg-zinc-600 transition duration-300 ease-in-out rounded-md"
+          onMouseEnter={handleCharacterHover}
+          onMouseLeave={handleCharacterHover}
+        >
+          <p>
+            {hoveredCharacter || isMobile ? (isMobile ? "Tap" : "Click") + " for know more about characters" : "Characters"}
+          </p>
+        </div>
+      </Link>
+    </main>
+  );
+}
