a776862d33f042aadd872669639f44f9316239e4
SaniEHerr <santiagoeherrera@gmail.com>
2024-05-14 15:53:14 -0300

    Refactor: changing some styles from charactersPage

--- app/(site)/_components/CharactersCards.tsx
+++ app/(site)/_components/CharactersCards.tsx
@@ -6,27 +6,35 @@ interface CharactersCardsProps {
   results : CharacterData[];
 }
 
+const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 const CharactersCards = ({ results }: CharactersCardsProps) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
       {results.map((character: CharacterData) => (
-        <Link
-          href={`/characters/${character.character_id}`}
-          key={character.name}
-          className="bg-zinc-700 bg-opacity-60 backdrop-filter backdrop-blur-sm shadow-md rounded-lg overflow-hidden"
-        >
-          <div className="p-4">
-            <h2 className="text-xl font-bold mb-2">{character.name}</h2>
+        <div key={character.name} className="bg-zinc-700 bg-opacity-60 backdrop-filter backdrop-blur-sm shadow-md rounded-lg overflow-hidden p-4 flex flex-col justify-between gap-3">
+          <div className='flex flex-col gap-3 items-center'>
+            <h2 className="text-2xl font-bold text-center truncate">{character.name}</h2>
             <Image
               src={character.image}
               width={204}
-              height={362}
+              height={285.25}
               alt="Character picture"
             />
-            {character.eye_color && <p className="">Eye color: {character.eye_color}</p>}
-            {character.gender && <p className="">Gender: {character.gender}</p>}
+            <div className='flex flex-col items-center text-lg'>
+              {character.eye_color && <p className="font-semibold">Eye color: <span className='font-normal'>{capitalizeFirstLetter(character.eye_color)}</span> </p>}
+              {character.gender && <p className="font-semibold">Gender: <span className='font-normal'>{capitalizeFirstLetter(character.gender)}</span></p>}
+            </div>
           </div>
-        </Link>
+
+          <div className='flex justify-center'>
+            <Link href={`/characters/${character.character_id}`}>
+              <p className='text-base hover:text-[#FFE81F] transition-colors duration-300 font-semibold md:font-normal underline md:no-underline hover:underline hover:font-semibold'>Know more</p>
+            </Link>
+          </div>
+        </div>
       ))}
     </div>
   )
