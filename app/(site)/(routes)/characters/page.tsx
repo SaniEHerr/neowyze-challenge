f21be6fb504fc37dc298dcca0418e68f06cd7d1b
SaniEHerr <santiagoeherrera@gmail.com>
2024-05-14 13:32:08 -0300

    Refactor: basic charactersPage structure

--- app/(site)/(routes)/characters/page.tsx
+++ app/(site)/(routes)/characters/page.tsx
@@ -1,7 +1,23 @@
 const CharactersPage = () => {
+  
   return (
-    <div>
-      CharactersPage
+    <div className="max-w-[1420px] mx-auto">
+      
+      <div>
+        Filter component
+      </div>
+
+      <div className="flex flex-col gap-8">
+        <h2 className="text-4xl text-center font-bold font-starjhol underline">Characters</h2>
+        <div>
+          Characters Cards component
+        </div>
+      </div>
+      
+      <div>
+        Pagination component
+      </div>
+
     </div>
   )
 }
