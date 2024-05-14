95e0cab9a96b6d97d5ded573e6f5ade31169788e
SaniEHerr <santiagoeherrera@gmail.com>
2024-05-13 22:43:24 -0300

    Refactor: filmDetail styles

--- app/(site)/(routes)/films/[filmId]/page.tsx
+++ app/(site)/(routes)/films/[filmId]/page.tsx
@@ -22,45 +22,56 @@ const FilmByIdPage = async ({params}: { params: FilmParams }) => {
   const filmDetails: FilmDetails = await getFilmDetails(params.filmId)
   
   return (
+    <div className="flex flex-col gap-10 py-7">
+      <div className="flex flex-col justify-center items-center gap-4 bg-zinc-700 bg-opacity-60 backdrop-filter backdrop-blur-sm p-4 rounded-lg">
+        <div className="flex flex-col justify-center items-center gap-1">
+          <h1 className="text-4xl font-bold font-starjedi text-center">{filmDetails.title}</h1>
+          <p className="font-bold text-2xl font-starjedi">Episode {filmDetails.episode_id}</p>
+          <p className="text-xl">Directed by <span className="font-bold">{filmDetails.director}</span></p>
+        </div>
 
-    <div>
-      <h1>{filmDetails.title}</h1>
-      <p>Episode: {filmDetails.episode_id}</p>
-      <p>Director: {filmDetails.director}</p>
-      <Image
-        src={filmDetails.image}
-        width={204}
-        height={301}
-        alt="Film image"
-      />
-      <h2>Characters:</h2>
-      <ul className="flex flex-wrap gap-4">
+        <Image
+          src={filmDetails.image}
+          width={204}
+          height={301}
+          alt="Film image"
+        />
+      </div>
 
-        {filmDetails.characters.map((character: CharacterData, index: any) => {
-          const customUrl = `/characters/${character.characterId}`;
+      <div className="flex flex-col gap-4">
+        <h2 className="text-2xl font-bold font-starjedi text-center">Characters</h2>
 
-          return (
-            <div className="flex" key={index}>
-              <li className="bg-red-400 flex flex-col">
-                <div className="rounded-full overflow-hidden w-11 h-11">
-                  <Image
-                    src={character.image}
-                    width={204}
-                    height={362}
-                    alt="Character picture"
-                  />
-                </div>
-                <Link href={customUrl}>
-                  {character.name}
-                </Link>
-                <div>
-                  <Link href={customUrl}>Details</Link> 
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-3">
+          {filmDetails.characters.map((character: CharacterData, index: any) => {
+            const customUrl = `/characters/${character.characterId}`;
+
+            return (
+              <li key={index} className="bg-zinc-700 bg-opacity-60 backdrop-filter backdrop-blur-sm p-4 rounded-lg">
+                <div className="flex items-center">
+                  <div className="w-16 h-16 rounded-full overflow-hidden">
+                    <Image
+                      src={character.image}
+                      width={64}
+                      height={64}
+                      alt="Character picture"
+                    />
+                  </div>
+
+                  <div className="ml-4">
+                    <Link href={customUrl}>
+                      <p className="text-white font-bold">{character.name}</p>
+                    </Link>
+                    
+                    <Link href={customUrl}>
+                      <p className="text-gray-400 hover:text-gray-300 hover:underline">More Info</p>
+                    </Link>
+                  </div>
                 </div>
               </li>
-            </div>
-          );
-        })}
-      </ul>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
